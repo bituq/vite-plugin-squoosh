e3400131343718c3226dc2caf8a9bb4a1bfbef74
Dylan <d.noorland9@hotmail.com>
2022-11-16 12:32:16 +0100

    feat: change example

--- packages/examples/example-1/src/App.vue
+++ packages/examples/example-1/src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <main>
     <img src="@/assets/test.jpg"/>
-    <img src="@/assets/test.png"/>
-    <img src="/images/static-test.jpg"/>
-    <img src="/images/static-test.png"/>
   </main>
 </template>
--- packages/examples/example-1/vite.config.ts
+++ packages/examples/example-1/vite.config.ts
@@ -6,11 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), squooshPlugin({ codecs: {
-    oxipng: {
-      level: 3
+    mozjpeg: {
+      quality: 10
     }
-  }
-  })],
+  }})],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
